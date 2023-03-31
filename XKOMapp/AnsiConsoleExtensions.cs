bc0a03c759ac6410457144ede70d4794779fc71c
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-04-01 00:57:04 +0200

    changed Standard Renderables to getters

--- XKOMapp/AnsiConsoleExtensions.cs
+++ XKOMapp/AnsiConsoleExtensions.cs
@@ -4,6 +4,6 @@ using Spectre.Console.Rendering;
 
 public static class AnsiConsoleExtensions
 {
-    public static IRenderable StandardHeader() => new Rule("[#b45ae0]X[/]-KOM");
-    public static IRenderable StandardLine() => new Rule().HeavyBorder().RuleStyle(Style.Parse("#0e8f75"));
+    public static IRenderable StandardHeader => new Rule("[#b45ae0]X[/]-KOM");
+    public static IRenderable StandardLine => new Rule().HeavyBorder().RuleStyle(Style.Parse("#0e8f75"));
 }
