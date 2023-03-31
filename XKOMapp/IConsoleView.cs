a343b8ade01f8afc64684c18bf14a9ab0531fd17
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-03-31 12:08:46 +0200

    added ICosnoleView interface

---
+++ XKOMapp/IConsoleView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XKOMapp
+{
+    public interface IConsoleView
+    {
+        public void PrintView();
+    }
+}
