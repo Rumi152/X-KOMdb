3171831d4c2081b94087ddd6c0231f3623c2210c
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-03-31 12:04:43 +0200

    added AnsiConsoleExtensions with StandardHeader method

---
+++ XKOMapp/AnsiConsoleExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Spectre.Console;
+
+public static class AnsiConsoleExtensions
+{
+    public static void StandardHeader()
+    {
+        AnsiConsole.Write(new Rule("[#b45ae0]X[/]-KOM"));
+    }
+}
