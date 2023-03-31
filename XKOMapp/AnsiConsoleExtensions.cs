428368e52c9e237925e68df2d9a0dc9bd407e2c9
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-04-01 01:22:47 +0200

    documented code

--- XKOMapp/AnsiConsoleExtensions.cs
+++ XKOMapp/AnsiConsoleExtensions.cs
@@ -5,9 +5,21 @@ using XKOMapp.GUI;
 
 public static class AnsiConsoleExtensions
 {
+    /// <summary>
+    /// Standard header IRenderable
+    /// </summary>
     public static IRenderable StandardHeader => new Rule("[#b45ae0]X[/]-KOM");
+
+    /// <summary>
+    /// Standard line separator IRenderable
+    /// </summary>
     public static IRenderable StandardLine => new Rule().HeavyBorder().RuleStyle(Style.Parse("#0e8f75"));
 
+    /// <summary>
+    /// Converts IRenderable to simplest form of Console row
+    /// </summary>
+    /// <param name="renderable">IRenderable to convert</param>
+    /// <returns>ConsoleRow without interaction and onHover stylization</returns>
     public static ConsoleRow ToConsoleRow(this IRenderable renderable)
     {
         return new ConsoleRow(renderable);
