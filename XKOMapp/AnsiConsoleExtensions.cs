7c71659ea06fddde211cd0caad8805956bee4f79
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-04-01 01:03:13 +0200

    added extension to IRenderable to convert it it ConsoleRow

--- XKOMapp/AnsiConsoleExtensions.cs
+++ XKOMapp/AnsiConsoleExtensions.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using Spectre.Console;
 using Spectre.Console.Rendering;
+using XKOMapp.GUI;
 
 public static class AnsiConsoleExtensions
 {
     public static IRenderable StandardHeader => new Rule("[#b45ae0]X[/]-KOM");
     public static IRenderable StandardLine => new Rule().HeavyBorder().RuleStyle(Style.Parse("#0e8f75"));
+
+    public static ConsoleRow ToConsoleRow(this IRenderable renderable)
+    {
+        return new ConsoleRow(renderable);
+    }
 }
