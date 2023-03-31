9ee5094791bfe6d4bf7bf24b7561a761968f8011
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-04-01 01:15:41 +0200

    documented code

--- XKOMapp/GUI/ConsoleRow.cs
+++ XKOMapp/GUI/ConsoleRow.cs
@@ -14,6 +14,11 @@ public class ConsoleRow
     private IRenderable hoveredRenderContent;
     private readonly ConsoleRowAction interactionAction;
 
+    /// <summary>
+    /// Creates row with interaction
+    /// </summary>
+    /// <param name="renderContent">Content rendered to console</param>
+    /// <param name="interactionAction">Action on interaction</param>
     public ConsoleRow(IRenderable renderContent, ConsoleRowAction interactionAction)
     {
         this.renderContent = renderContent;
@@ -21,6 +26,12 @@ public class ConsoleRow
         this.interactionAction = interactionAction;
     }
 
+    /// <summary>
+    /// Creates row with interaction and onHover stylization
+    /// </summary>
+    /// <param name="renderContent">Content rendered to console</param>
+    /// <param name="hoveredRenderContent">Content rendered to console when hovered</param>
+    /// <param name="interactionAction">Action on interaction</param>
     public ConsoleRow(IRenderable renderContent, IRenderable hoveredRenderContent, ConsoleRowAction interactionAction)
     {
         this.renderContent = renderContent;
@@ -28,6 +39,10 @@ public class ConsoleRow
         this.interactionAction = interactionAction;
     }
 
+    /// <summary>
+    /// Creates row
+    /// </summary>
+    /// <param name="renderContent">Content rendered to console</param>
     public ConsoleRow(IRenderable renderContent)
     {
         this.renderContent = renderContent;
@@ -35,6 +50,11 @@ public class ConsoleRow
         interactionAction = null;
     }
 
+    /// <summary>
+    /// Creates row with onHover stylization
+    /// </summary>
+    /// <param name="renderContent">Content rendered to console</param>
+    /// <param name="hoveredRenderContent">Content rendered to console when hovered</param>
     public ConsoleRow(IRenderable renderContent, IRenderable hoveredRenderContent)
     {
         this.renderContent = renderContent;
@@ -42,7 +62,16 @@ public class ConsoleRow
         interactionAction = null;
     }
 
+    /// <summary>
+    /// Gets content to render
+    /// </summary>
+    /// <param name="hovered">Is row hovered on right now</param>
+    /// <returns>IRenderable to write to console</returns>
     public IRenderable GetRenderContent(bool hovered = false) => hovered ? hoveredRenderContent : renderContent;
+
+    /// <summary>
+    /// Method to ivoke when interacting with row
+    /// </summary>
     public void OnInteraction() => interactionAction?.Invoke(this);
 }
 
