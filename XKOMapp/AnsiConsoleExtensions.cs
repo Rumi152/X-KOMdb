f57cdf358df9075a8fa4c42446f40e5cc7c1b531
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-04-01 00:35:11 +0200

    added standard IRenderables

--- XKOMapp/AnsiConsoleExtensions.cs
+++ XKOMapp/AnsiConsoleExtensions.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using Spectre.Console;
+using Spectre.Console.Rendering;
 
 public static class AnsiConsoleExtensions
 {
-    public static void StandardHeader()
-    {
-        AnsiConsole.Write(new Rule("[#b45ae0]X[/]-KOM"));
-    }
+    public static IRenderable StandardHeader() => new Rule("[#b45ae0]X[/]-KOM");
+    public static IRenderable StandardLine() => new Rule().HeavyBorder().RuleStyle(Style.Parse("#0e8f75"));
 }
