caa6a940aa56baf75d3b8a2658e5be0cc86ffb5d
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-04-01 01:09:39 +0200

    documented Main

--- XKOMapp/Program.cs
+++ XKOMapp/Program.cs
@@ -11,34 +11,32 @@ namespace XKOMapp
             Console.CursorVisible = false;
             var printer = new ConsolePrinter();
 
-            printer.AddRow(AnsiConsoleExtensions.StandardHeader.ToConsoleRow());
-            printer.AddRow(new ConsoleRow(new Text("Yoo1")));
-            printer.AddRow(new ConsoleRow(new Text("Yoo2")));
-            printer.AddRow(new ConsoleRow(new Text("Yoo3")));
-            printer.AddRow(new ConsoleRow(new Text("Yoo4")));
-            printer.AddRow(AnsiConsoleExtensions.StandardLine.ToConsoleRow());
-            printer.StartContent();
+            printer.AddRow(AnsiConsoleExtensions.StandardHeader.ToConsoleRow()); //creates row with standard header
+            printer.AddRow(new ConsoleRow(new Text("Info")));
+            printer.AddRow(new ConsoleRow(new Text("Info2")));
+            printer.AddRow(AnsiConsoleExtensions.StandardLine.ToConsoleRow()); //creates row with standard separator line
+            printer.StartContent(); //starts interactible content
+
+            //row that is different when hovered
             printer.AddRow(new ConsoleRow(
-                new Text("Yoo5"),
-                new Markup("[yellow]Yoo5[/]")
+                new Text("White"),
+                new Markup("[yellow]Yellow[/]")
             ));
-            printer.AddRow(new ConsoleRow(new Text("Yoo6")));
+            printer.AddRow(new ConsoleRow(new Text("Text1")));
+            printer.AddRow(new ConsoleRow(new Markup("[red]Text2[/]"))); //stylized text
+            printer.AddRow(new ConsoleRow(new Text("Text3")));
+            printer.AddRow(new ConsoleRow(new Panel("Text4").HeavyBorder().Header("Ex"))); //panel
+            printer.AddRow(new ConsoleRow(new Text("Text5")));
 
-            printer.AddRow(new ConsoleRow(new Markup("[red]Yooo7[/]")));
-
-            printer.AddRow(new ConsoleRow(new Text("Yoo8")));
-
-            printer.AddRow(new ConsoleRow(new Panel("Yooo9 (Prequel)").HeavyBorder().Header("Mario")));
-
-            printer.AddRow(new ConsoleRow(new Text("Yoo10")));
-
+            //row with interaction on clicked enter
             printer.AddRow(new ConsoleRow(
                 new Panel("Yooo11 (Sequel)").RoundedBorder().Header("Luigi"),
                 (row) => printer.AddRow(new ConsoleRow(new Text("no one expected the spanish inquisition")))));
 
-            printer.ReloadBuffer();
-            printer.PrintBuffer();
+            printer.ReloadBuffer(); //reloading buffers from rows list
+            printer.PrintBuffer(); //rendering from buffers
 
+            //checking for input in loop
             while (true)
             {
                 if (Console.KeyAvailable)
@@ -63,6 +61,7 @@ namespace XKOMapp
                             break;
                     }
 
+                    //refreshing screen
                     printer.ClearScreen();
                     printer.ReloadBuffer();
                     printer.PrintBuffer();
