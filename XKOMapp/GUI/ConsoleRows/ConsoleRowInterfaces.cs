5e16268c0ee7bceb62204e2ad75fd313f4ee290c
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-04-01 22:51:52 +0200

    documented code

--- XKOMapp/GUI/ConsoleRows/ConsoleRowInterfaces.cs
+++ XKOMapp/GUI/ConsoleRows/ConsoleRowInterfaces.cs
@@ -2,28 +2,43 @@
 
 namespace XKOMapp.GUI.ConsoleRows;
 
+/// <summary>
+/// Basic renderable ConsoleRow
+/// </summary>
 public interface IConsoleRow
 {
     IRenderable GetRenderContent();
 }
 
+/// <summary>
+/// ConsoleRow with action on click
+/// </summary>
 public interface IInteractableConsoleRow : IConsoleRow
 {
     void OnInteraction(ConsolePrinter printer);
 }
 
+/// <summary>
+/// ConsoleRow with actions on hovering start and end
+/// </summary>
 public interface IHoverConsoleRow : IConsoleRow
 {
     void OnHoverStart();
     void OnHoverEnd();
 }
 
+/// <summary>
+/// ConsoleRow with custom displayed cursor
+/// </summary>
 public interface ICustomCursorConsoleRow : IConsoleRow
 {
     string GetCustomCursor();
     string GetCustomCursorBackground();
 }
 
+/// <summary>
+/// ConsoleRow which can be hidden
+/// </summary>
 public interface IHideableConsoleRow : IConsoleRow
 {
     public bool IsHidden { get; protected set; }
@@ -49,4 +64,9 @@ public interface IHideableConsoleRow : IConsoleRow
     protected void OnShow();
 }
 
+/// <summary>
+/// Delegate for action invoked by ConsoleRow
+/// </summary>
+/// <param name="row">Row invoking action</param>
+/// <param name="printer">Printer owning row</param>
 public delegate void ConsoleRowAction(IConsoleRow row, ConsolePrinter printer);
