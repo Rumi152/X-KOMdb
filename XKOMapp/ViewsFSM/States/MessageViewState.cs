d4480e5989047167acb78c95baea9633be0d3cee
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-05-29 17:30:26 +0200

    added MessageViewState

---
+++ XKOMapp/ViewsFSM/States/MessageViewState.cs
@@ -0,0 +1,76 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XKOMapp.GUI;
+using XKOMapp.GUI.ConsoleRows;
+
+namespace XKOMapp.ViewsFSM.States;
+
+internal class MessageViewState : ViewState
+{
+    private readonly string message;
+    private readonly ViewState buttonTarget;
+    private readonly string buttonMessage;
+
+    public MessageViewState(ViewStateMachine stateMachine, string message) : base(stateMachine)
+    {
+        this.message = message;
+        this.buttonTarget = fsm.GetSavedState("mainMenu");
+        this.buttonMessage = "Back to main menu";
+    }
+
+    public MessageViewState(ViewStateMachine stateMachine, string message, ViewState buttonTarget, string buttonMessage) : base(stateMachine)
+    {
+        this.message = message;
+        this.buttonTarget = buttonTarget;
+        this.buttonMessage = buttonMessage;
+    }
+
+    protected override void InitialPrinterBuild(ConsolePrinter printer)
+    {
+        printer.AddRow(StandardRenderables.StandardHeader.ToBasicConsoleRow());
+        printer.StartContent();
+
+        printer.AddRow(new InteractableConsoleRow(new Markup(buttonMessage), (row, owner) => fsm.Checkout(buttonTarget)));
+        //printer.AddRow(new InteractableConsoleRow(new Markup("[red]Exit app[/]"), (row, owner) => Environment.Exit(0)));
+        GetWrappedMessage(message).ForEach(x => printer.AddRow(new Text(x).ToBasicConsoleRow()));
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+
+        printer.ResetCursor();
+    }
+
+    //REFACTOR maybe add it to ConsolePrinter
+    private static List<string> GetWrappedMessage(string message)
+    {
+        var text = message.ReplaceLineEndings(" ");
+        List<string> lines = new();
+
+        while (text.Length > 0)
+        {
+            const string leftPad = "  ";
+            int width = Math.Max(32, Console.WindowWidth - 8);
+
+            if (text.Length <= width)
+            {
+                lines.Add(leftPad + text.Trim());
+                break;
+            }
+
+            int takenLength = new string(text.Take(width).ToArray()).LastIndexOf(' ');
+            if (takenLength++ == -1) takenLength = width;
+
+            lines.Add(leftPad + new string(text.Take(takenLength).ToArray()).Trim());
+            text = new(text.Skip(takenLength).ToArray());
+        }
+
+        return lines;
+    }
+}
+
