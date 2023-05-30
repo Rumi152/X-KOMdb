7178d247942714cff4abda752c0bc1a8ff8f978d
Nesittis <nesittis@gmail.com>
2023-05-30 18:03:00 +0200

    added number input field

--- XKOMapp/ViewsFSM/States/ProductListViewState.cs
+++ XKOMapp/ViewsFSM/States/ProductListViewState.cs
@@ -8,11 +8,13 @@ using System.Threading.Tasks;
 using XKOMapp.GUI.ConsoleRows;
 using XKOMapp.GUI;
 using XKOMapp.Models;
+using XKOMapp.GUI.ConsoleRows.ProductList;
 
 namespace XKOMapp.ViewsFSM.States;
 internal class ProductListViewState : ViewState
 {
     private readonly Product product;
+    private ProductNumberInputConsoleRow numberInput = null!;
     public ProductListViewState(ViewStateMachine stateMachine, Product product) : base(stateMachine)
     {
         this.product = product;
@@ -28,6 +30,9 @@ internal class ProductListViewState : ViewState
         }));
         printer.AddRow(new Rule("Lists").RuleStyle(Style.Parse(StandardRenderables.AquamarineColorHex)).HeavyBorder().ToBasicConsoleRow());
         printer.EnableScrolling();
+
+        numberInput = new ProductNumberInputConsoleRow($"Number of products: ", 32);
+        printer.AddRow(numberInput);
 
         printer.AddRow(new InteractableConsoleRow(new Text("Add new list"), (row, owner) =>
         {
@@ -87,12 +92,15 @@ internal class ProductListViewState : ViewState
                     ));
                     return;
                 }
+
+                int number = Convert.ToInt32(numberInput.CurrentInput);
                 //todo czekboksy
                 using var context = new XkomContext();
                 var newProdList = new ListProduct()
                 {
                     ProductId = product.Id,
                     ListId = x.Id,
+                    Number = number
                 };
                 context.Add(newProdList);
                 context.SaveChanges();
