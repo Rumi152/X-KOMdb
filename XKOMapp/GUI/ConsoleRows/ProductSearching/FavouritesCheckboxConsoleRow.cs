a14fafb514faa7c36d64c9146cc54814cb2b8344
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-05-30 20:45:00 +0200

    added searching only favourite products

---
+++ XKOMapp/GUI/ConsoleRows/ProductSearching/FavouritesCheckboxConsoleRow.cs
@@ -0,0 +1,43 @@
+﻿using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace XKOMapp.GUI.ConsoleRows.ProductSearching;
+
+internal class FavouritesCheckboxConsoleRow : IInteractableConsoleRow, ICustomCursorConsoleRow
+{
+    private readonly string preMarkupLabel;
+    private readonly Action onInteraction;
+    private ConsolePrinter owner = null!;
+
+    public bool IsChecked { get; private set; } = false;
+
+    public FavouritesCheckboxConsoleRow(string preMarkupLabel, Action onInteraction)
+    {
+        this.preMarkupLabel = preMarkupLabel;
+        this.onInteraction = onInteraction;
+    }
+
+    public IRenderable GetRenderContent()
+    {
+        string checkbox = IsChecked ? $"[{StandardRenderables.GoldColorHex}][[X]][/]" : "[[]]";
+        return new Markup($"{preMarkupLabel}{checkbox}");
+    }
+
+    public void SetOwnership(ConsolePrinter owner) => this.owner = owner;
+
+    public string GetCustomCursor() => "»";
+    public string GetCustomCursorBackground() => " ";
+
+    public void OnInteraction()
+    {
+        owner.SetBufferDirty();
+        IsChecked = !IsChecked;
+        onInteraction();
+    }
+
+    public void Uncheck()
+    {
+        owner.SetBufferDirty();
+        IsChecked = false;
+    }
+}
