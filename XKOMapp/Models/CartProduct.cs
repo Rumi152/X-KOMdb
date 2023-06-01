38339cba96966c3446d10d9bc1cd4ef7169c85cd
Nesittis <nesittis@gmail.com>
2023-06-01 15:34:47 +0200

    did scaffold and deleted warning

--- XKOMapp/Models/CartProduct.cs
+++ XKOMapp/Models/CartProduct.cs
@@ -11,6 +11,8 @@ public partial class CartProduct
 
     public int CartId { get; set; }
 
+    public int Amount { get; set; }
+
     public virtual Cart Cart { get; set; } = null!;
 
     public virtual Product Product { get; set; } = null!;
